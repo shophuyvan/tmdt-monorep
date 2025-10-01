98481d08b6f4f7d1acc5569467ac9035b8ddf73e
shophuyvan <kinhdoanhonlinegiasoc@gmail.com>
2025-10-01 08:42:23 +0700

    hotfix(api): login/products raw-query fallbacks; postinstall prisma generate; add prisma seed

---
+++ tmdt-monorep-main/api/lib/prisma.js
@@ -0,0 +1,16 @@
+// api/lib/prisma.js
+const { PrismaClient } = require('@prisma/client');
+
+let prisma;
+if (!global._prisma) {
+  prisma = new PrismaClient({
+    log: ['warn', 'error'],
+  });
+  if (process.env.NODE_ENV !== 'production') {
+    global._prisma = prisma;
+  }
+} else {
+  prisma = global._prisma;
+}
+
+module.exports = prisma;
